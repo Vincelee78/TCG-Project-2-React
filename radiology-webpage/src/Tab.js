da0d978a1c420553706069aac0e4a197711cd805
Vincelee78 <vincelee7778@gmail.com>
2021-10-09 07:12:54 +0000

    added navtab contents

--- radiology-webpage/src/Tab.js
+++ radiology-webpage/src/Tab.js
@@ -3,52 +3,173 @@ import Listing from './components/Listing'
 import AddNew from './components/AddNew'
 
 
+
 export default class Tab extends React.Component {
-    state = {
-        'active': 'listing'  // indicate which page is active
-    }
+  state = {
+    'active': 'listing',  // indicate which page is active
+    // 'data': [
+    //   {
+    //     '_id': 1,
+    //     'title': 'Chicken Rice',
+    //     'ingredients': [
+    //       'Chicken Broth',
+    //       'Chicken',
+    //       'Rice'
+    //     ]
+    //   },
+    //   {
+    //     '_id': 2,
+    //     'title': 'Duck Rice',
+    //     'ingredients': [
+    //       'Duck',
+    //       'Rice'
+    //     ]
+    //   }
+    // ],
 
-    render() {
-        return <React.Fragment>
-            <ul className="nav nav-tabs">
-                <li className="nav-item">
-                    <button className="nav-link active" 
-                        aria-current="page"
-                        onClick={()=>{
-                            this.setActive('listing')
-                        }}
-                        >Case Title</button>
-                </li>
-                <li className="nav-item">
-                    <button className="nav-link">Case Findings</button>
-                </li>
-                <li className="nav-item">
-                    <button className={"nav-link"}
-                        onClick={()=>{
-                            this.setActive('addnew')
-                        }}
-                    >Add New Case</button>
-                </li>               
-            </ul>
-            {this.renderContent()}
-        </React.Fragment>
-    }
 
-    setActive(nextPage) {
-        this.setState({
-            'active': nextPage
-        })
-    }
 
-    afterAddNewRecipe = () => {
-        this.setActive('listing')
-    }
+    'data': [
+      {
+        "patientId": 1,
+        "gender": "Male",
+        "DOB": "2000/07/28",
+        "bodySystems": [
+          "Skeletal",
+          "Cardiovascular"
+        ],
 
-    renderContent() {
-        if (this.state.active =='listing') {
-            return <Listing/>
-        } else if (this.state.active == 'addnew') {
-            return <AddNew onAfterAddRecipe={this.afterAddNewRecipe}/>
-        }
+        "radiologistId": "A01",
+        "studentsTagged": [
+          "T01",
+          "T02",
+          "T03"
+        ],
+
+        "clinicalHistory": "High Blood Pressure",
+        "signsSymptomsTitle": "Distressed. Sharp pain and swelling on chest area",
+        "images": <img src='https://pubs.rsna.org/cms/10.1148/ryct.2020200280/asset/images/medium/ryct.2020200280.fig3.gif'/>,
+        "publishedDate": "2021-10-07",
+
+        "scientificReferences": "Kang E. Ribs: a spectrum of abnormalities.(2002)",
+        "caseDiscussion": "AP view of chest. Prominent central vascular markings. Clear costophrenic angles.13 pairs of ribs.",
+
+        "modality": [
+          "X-ray",
+          "MRI",
+          "CT"
+        ]
+      },
+
+
+      // {
+      //   "patientId": 2,
+      //   "gender": "Female",
+      //   "DOB": "1998/04/19",
+      //   "bodySystems": [
+      //     "Skeletal",
+      //     "Muscular"
+      //   ],
+
+
+      //   "radiologistId": "A02",
+      //   "studentsTagged": [
+      //     "T04",
+      //     "T05",
+      //     "T06"
+      //   ],
+      //   "clinicalHistory": "Diabetes",
+      //   "signsSymptomsTitle": "Swelling, bruising and tenderness of 3rd and 4th fingers of left hand. Deformity and pain present",
+      //   "images": "url",
+      //   "publishedDate": "2021-06-07",
+
+      //   "scientificReferences": "Fractures of the scaphoid Onur Berber et al., The BMJ: Research, 2020",
+      //   "caseDiscussion": "Fracture at the base of the 3rd and 4th middle phalanx. The fracture is almost impossible to see on the AP view. The oblique view gives the best view of the fracture. The oblique view gives the best view of the fracture. This case highlights the importance of using two views in patients with trauma, namely AP and Oblique view.",
+      //   "modality": [
+      //     "X-ray"
+      //   ]
+      // },
+
+
+      // {
+      //   "patientId": 3,
+      //   "gender": "Female",
+      //   "DOB": "1995/02/20",
+      //   "bodySystems": [
+      //     "Nervous"
+      //   ],
+
+
+      //   "radiologistId": "A03",
+      //   "studentsTagged": [
+      //     "T07",
+      //     "T08",
+      //     "T09"
+      //   ],
+      //   "clinicalHistory": "Development delay",
+      //   "signsSymptomsTitle": "New-onset seizures",
+      //   "images": "url",
+      //   "publishedDate": "2020-02-08",
+
+      //   "scientificReferences": "Changes of gray matter structure of patients with chronic insomnia in magnetic resonance imaging Qin He et al., Chinese Medical Journals Publishing House Co., Ltd., 2021",
+      //   "caseDiscussion": "Selected MRI images demonstrate an abnormal white matter, particularly posteriorly and in the periventricular region. It is characterised by bilateral symmetric deep white matter hyperintensity on T2 weighted images, with associated involvement of the subcortical white matter, which results in poor grey-white matter differentiation on T2 weighted images. MRS demonstrates elevation of choline (Cho). Alpha-glutamate peak is not visible. Features are consistent with subsequently established 18q syndrome.",
+      //   "modality": [
+      //     "MRI"
+      //   ],
+      // }
+
+    ],
+
+
+    loading: false
+  }
+
+
+
+
+  render() {
+    return <React.Fragment>
+      <div id='tabcss'>
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <button className="nav-link active"
+              aria-current="page"
+              onClick={() => {
+                this.setActive('listing')
+              }}
+            >Case Title</button>
+          </li>
+          <li className="nav-item">
+            <button className="nav-link">Case Findings</button>
+          </li>
+          <li className="nav-item">
+            <button className={"nav-link"}
+              onClick={() => {
+                this.setActive('addnew')
+              }}
+            >Add New Case</button>
+          </li>
+        </ul>
+        {this.renderContent()}
+      </div>
+    </React.Fragment>
+  }
+
+  setActive(nextPage) {
+    this.setState({
+      'active': nextPage
+    })
+  }
+
+  afterAddNewRecipe = () => {
+    this.setActive('listing')
+  }
+
+  renderContent() {
+    if (this.state.active == 'listing') {
+      return <Listing data={this.state.data} />
+    } else if (this.state.active == 'addnew') {
+      return <AddNew onAfterAddRecipe={this.afterAddNewRecipe} />
     }
+  }
 }
--- radiology-webpage/src/components/Listing.js
+++ radiology-webpage/src/components/Listing.js
@@ -1,43 +1,90 @@
 import React from 'react'
 import axios from 'axios'
-export default class Listing extends React.Component {
-    
-    url = "https://8888-copper-tahr-20npvdcv.ws-us18.gitpod.io/"
-    state = {
-        'data': [
-           
-        ],
-        loading:false
-    }
+// export default class Listing extends React.Component {
 
-    async componentDidMount() {
-        this.setState({
-            'loading':true
-        })
-        await this.fetchData();
-        this.setState({
-            'loading':false
-        })
-    }
+// url = "https://8888-copper-tahr-20npvdcv.ws-us18.gitpod.io/"
 
-    fetchData = async () => {
-        let response = await axios.get(this.url + "recipes")
-        this.setState({
-            'data': response.data
-        })
-    }
+// state = {
+//     'data': [ 
 
-    render() {
-        return <React.Fragment>
-            {this.state.data.map( recipe => <div className="card my-3" key={recipe._id}>
-                <div className="card-body">
-                    <h3 className="card-title">{recipe.title}</h3>
-                    <h4>Ingredients</h4>
-                    <ul>
-                        {recipe.ingredients.map( ingredient => <li key={ingredient}>{ingredient}</li>)}
-                    </ul>
+//     ],
+//     loading:false
+// }
+
+export default function Listing(props) {
+    return <React.Fragment>
+        <div className="container">
+            <h2 style={{ color: 'brown' }}>Featured Case</h2>
+            {props.data.map(r => <React.Fragment key={r.patientId}>
+                <div className="card">
+                    <div className="card-body">
+                        <h3 className="card-title">
+                            <h5>Case presentation:</h5>
+                            <h6>{r.signsSymptomsTitle}</h6>
+                        </h3>
+                        <h5>Patient ID: {r.patientId}</h5>
+                        <h5>Gender: {r.gender}</h5>
+                        <h5>Date of birth: {r.DOB}</h5>
+                        <div id='flexContainer'>
+                        <h5 id='caseImgUrl'>{r.images}</h5>
+                        <div id='contents'>
+                        <h5>Clinical History: {r.clinicalHistory}</h5>
+                        <h5>Imaging Modality:</h5>
+                        <ul>
+                            {r.modality.map(i => <li key={i}>{i}</li>)}
+                        </ul>
+                        <h5>Published Date: {r.publishedDate}</h5>
+                        <h5>Scientific References: {r.scientificReferences}</h5>
+                        <h5>Case Discussion: {r.caseDiscussion}</h5>
+                        <h5>Radiologist ID: {r.radiologistId}</h5>
+                        <h5>Students Tagged IDs:</h5>
+                        <ul>
+                            {r.studentsTagged.map(i => <li key={i}>{i}</li>)}
+                        </ul>
+                        <h5>Body Systems:</h5>
+                        <ul>
+                            {r.bodySystems.map(i => <li key={i}>{i}</li>)}
+                        </ul>
+                        </div>
+                    </div>
+                  </div>
                 </div>
-            </div>)}
-        </React.Fragment>
-    }
+            </React.Fragment>)}
+        </div>
+    </React.Fragment>
 }
+
+
+
+
+    // async componentDidMount() {
+    //     this.setState({
+    //         'loading':true
+    //     })
+    //     await this.fetchData();
+    //     this.setState({
+    //         'loading':false
+    //     })
+    // }
+
+    // fetchData = async () => {
+    //     let response = await axios.get(this.url + "recipes")
+    //     this.setState({
+    //         'data': response.data
+    //     })
+    // }
+
+    // render() {
+    //     return <React.Fragment>
+    //         {this.state.data.map( recipe => <div className="card my-3" key={recipe._id}>
+    //             <div className="card-body">
+    //                 <h3 className="card-title">{recipe.title}</h3>
+    //                 <h4>Ingredients</h4>
+    //                 <ul>
+    //                     {recipe.ingredients.map( ingredient => <li key={ingredient}>{ingredient}</li>)}
+    //                 </ul>
+    //             </div>
+    //         </div>)}
+    //     </React.Fragment>
+    // }
+// }
