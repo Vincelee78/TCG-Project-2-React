3995d0226108076ba8ab77e57df677f100fdc932
Vincelee78 <vincelee7778@gmail.com>
2021-10-19 16:56:24 +0000

    added edit cases on all cases content

--- radiology-webpage/src/components/AllCasesContent.js
+++ radiology-webpage/src/components/AllCasesContent.js
@@ -12,6 +12,19 @@ export default class AllCasesContent extends React.Component {
         'data': [
 
         ],
+        userBeingEdited: 0,
+        modifiedpatientId: '',
+        modifiedGender: '',
+        modifieddob: '',
+        modifiedclinicalHistory: '',
+        modifiedmodality: '',
+        modifiedcaseDiscussion: '',
+        modifiedbodySystem: [],
+        modifiedscienticReferences: '',
+        modifiedpublishedDate: '',
+        modifiedsignSymptomsTitle: '',
+        modifiedimages: '',
+        modifiedradiologistId: '',
     }
 
     url = "https://5000-maroon-anglerfish-ugo6rg5n.ws-us17.gitpod.io/"
@@ -43,15 +56,46 @@ export default class AllCasesContent extends React.Component {
         })
     }
 
-    renderContent() {
-        if (this.state.active === 'errorMessage') {
-            return <ErrorMessage />
-        } if (this.state.active === 'addnew') {
-            return <AddNew onAfterAddPatient={this.afterAddNewPatient} />
-        } if (this.state.active === 'editCase') {
-            return <AllCasesContent />
+    updateanswer = (evt) => {
+        this.setState({
+            modifiedmodality: evt.target.value
+        })
+    }
+
+    updateitems = (evt) => {
+        if (this.state.modifiedbodySystem.includes(evt.target.value)) {
+            let clone = this.state.modifiedbodySystem.slice();
+            let index = this.state.modifiedbodySystem.indexOf(evt.target.value)
+            clone.splice(index, 1);
+
+            this.setState({
+                modifiedbodySystem: clone
+            })
+
+        } else {
+
+            let clone = this.state.modifiedbodySystem.slice();
+
+
+            clone.push(evt.target.value);
+
+
+            this.setState({
+                modifiedbodySystem: clone
+            })
         }
     }
+
+
+    // renderContent() {
+    //     if (this.state.active === 'errorMessage') {
+    //         return <ErrorMessage />
+    //     } if (this.state.active === 'addnew') {
+    //         return <AddNew onAfterAddPatient={this.afterAddNewPatient} />
+    //     } if (this.state.active === 'editCase') {
+    //         return <AllCasesContent />
+    //     }
+    // }
 
     afterAddNewPatient = () => {
         this.setActive('AllCasesContent')
@@ -80,19 +124,286 @@ export default class AllCasesContent extends React.Component {
         //   console.log(modifiedCase)
       };
     
+      beginEdit = (patientsData) => {
+        this.setState({
+            userBeingEdited: patientsData._id,
+            modifiedpatientId: patientsData.patientID,
+            modifiedGender: patientsData.gender,
+            modifieddob: patientsData.dob,
+            modifiedclinicalHistory: patientsData.clinicalHistory,
+            modifiedmodality: patientsData.modality,
+            modifiedpublishedDate: patientsData.publishedDate,
+            modifiedcaseDiscussion: patientsData.caseDiscussion,
+            modifiedbodySystem: patientsData.bodySystems,
+            modifiedscienticReferences: patientsData.scientificReferences,
+            modifiedimages: patientsData.images,
+            modifiedsignSymptomsTitle: patientsData.signsSymptomsTitle,
+            modifiedradiologistId: patientsData.radiologistId,
+        });
+    };
+
+    async updateCase() {
+        // clone the original task
+        // let currentUser = this.state.users.filter((a) => a._id === user._id)[0];
+        // modifiedUser is the cloned original array
+        let modifiedCase = this.state.data.slice();
+        modifiedCase._id = this.state.userBeingEdited;
+        await axios.put(this.url + 'patientsData1/' + modifiedCase._id, {
+            
+
+            // make changes to the clone
+            patientID: this.state.modifiedpatientId,
+            signsSymptomsTitle: this.state.modifiedsignSymptomsTitle,
+            gender: this.state.modifiedGender,
+            dob: this.state.modifieddob,
+            clinicalHistory: this.state.modifiedclinicalHistory,
+            modality: this.state.modifiedmodality,
+            caseDiscussion: this.state.modifiedcaseDiscussion,
+            bodySystems: this.state.modifiedbodySystem,
+            scientificReferences: this.state.modifiedscienticReferences,
+            publishedDate: this.state.modifiedpublishedDate,
+            images: this.state.modifiedimages,
+            radiologistId: this.state.modifiedradiologistId
+
+        })
+
+        let response = await axios.get(this.url + 'patientsData1/' + modifiedCase._id)
+        let modifiedCases = response.data
+
+        let indexToModify = this.state.data.findIndex(
+            (u) => u._id === modifiedCase._id
+        );
+        // clone the task array and insert the cloned task into the cloned array
+
+        let cloned = [
+            ...this.state.data.slice(0, indexToModify),
+            modifiedCases,
+            ...this.state.data.slice(indexToModify + 1)
+        ];
+
+        this.setState({
+            data: cloned,
+            userBeingEdited: 0,
+            active: 'AllCasesContent'
+        })
+
+    }
+
+      renderEditDisplay = (a) => {
+        return (
+
+            <React.Fragment>
+                <h3>Editing PatientID: {this.state.modifiedpatientId}</h3>
+                <div className="innerbox" style={{ textAlign: 'left' }}>
+                    <h6>Case Presentation:</h6>
+                    <textarea rows="4" cols="40" value={this.state.modifiedsignSymptomsTitle} name="modifiedsignSymptomsTitle"
+                        onChange={(evt) => {
+
+                            this.setState({
+                                modifiedsignSymptomsTitle: evt.target.value
+                            });
+                        }} > </textarea><br />
+
+                    <h6>Gender:</h6>
+                    <input
+                        type="text"
+                        value={this.state.modifiedGender}
+                        onChange={(evt) => {
+                            this.setState({
+                                modifiedGender: evt.target.value
+                            });
+                        }}
+                        name="modifiedGender"
+                    />
+
+                    <h6>Date of Birth:</h6>
+                    <input
+                        type="text"
+                        value={this.state.modifieddob}
+                        onChange={(evt) => {
+                            this.setState({
+                                modifieddob: evt.target.value
+                            });
+                        }}
+                        name="modifieddob"
+                    />
+
+                    <h6>Clinical History:</h6>
+                    <textarea rows="4" cols="40"
+                        type="text"
+                        value={this.state.modifiedclinicalHistory}
+                        onChange={(evt) => {
+                            this.setState({
+                                modifiedclinicalHistory: evt.target.value
+                            });
+                        }}
+                        name="modifiedclinicalHistory"
+                    ></textarea>
+
+                    <h6>Images:</h6>
+                    <textarea rows="4" cols="40"
+                        type="text"
+                        value={this.state.modifiedimages}
+                        onChange={(evt) => {
+                            this.setState({
+                                modifiedimages: evt.target.value
+                            });
+                        }}
+                        name="modifiedimages"
+                    ></textarea>
+
+                    <h6>Modality:</h6>
+                    <ul >
+                        <li>
+                            <input name="X-ray" type="radio" value={this.state.modifiedmodality} onChange={this.updateanswer} checked={this.state.modifiedmodality === 'X-ray'} /><label>X-ray</label>
+                        </li>
+                        <li>
+                            <input name="ComputedTomography" type="radio" value={this.state.modifiedmodality} onChange={this.updateanswer} checked={this.state.modifiedmodality === 'ComputedTomography'} /><label>Computed Tomography</label>
+                        </li>
+                        <li>
+                            <input name="Ultrasound" type="radio" value={this.state.modifiedmodality} onChange={this.updateanswer} checked={this.state.modifiedmodality === 'Ultrasound'} /><label>Ultrasound</label>
+                        </li>
+                        <li>
+                            <input name="MRI" type="radio" value={this.state.modifiedmodality} onChange={this.updateanswer} checked={this.state.modifiedmodality === 'MRI'} /><label>MRI</label>
+                        </li>
+                    </ul>
+                    {/* <input */}
+                    {/* // type="text"
+                        // value={this.state.modifiedmodality}
+                        // onChange={(evt) => { */}
+                    {/* //     this.setState({ */}
+                    {/* //         modifiedmodality: evt.target.value
+                        //     });
+                        // }}
+                        // name="modifiedmodality" */}
+                    {/* /> */}
+
+                    <h6>Published Date:</h6>
+                    <input
+                        type="text"
+                        value={this.state.modifiedpublishedDate}
+                        onChange={(evt) => {
+                            this.setState({
+                                modifiedpublishedDate: evt.target.value
+                            });
+                        }}
+                        name="modifiedpublishedDate"
+                    />
+
+                    <br /><br />
+                    <h6>Case Discussion:</h6>
+                    <textarea rows="6" cols="40"
+                        type="text"
+                        value={this.state.modifiedcaseDiscussion}
+                        onChange={(evt) => {
+                            this.setState({
+                                modifiedcaseDiscussion: evt.target.value
+                            });
+                        }}
+                        name="modifiedcaseDiscussion"
+                    ></textarea>
+
+                    <h6>Radiologist Name:</h6>
+                    <input
+                        type="text"
+                        value={this.state.modifiedradiologistId}
+                        onChange={(evt) => {
+                            this.setState({
+                                modifiedradiologistId: evt.target.value
+                            });
+                        }}
+                        name="modifiedbodysystem"
+                    />
+
+                    <br /><br />
+                    <h6>Body Systems:</h6>
+                    <div>
+                        <input type="checkbox" name="modifiedbodysystem" value="Cardiovascular" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Cardiovascular')} /><label>Cardiovascular</label>
+
+                        <input type="checkbox" name="modifiedbodysystem" value="Gastrointestinal" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Gastrointestinal')} /><label>Gastrointestinal</label>
+
+                        <input type="checkbox" name="modifiedbodysystem" value="Endocrine" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Endocrine')} /><label>Endocrine</label>
+
+                        <input type="checkbox" name="modifiedbodysystem" value="Exocrine" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Exocrine')} /><label>Exocrine</label>
+
+                        <input type="checkbox" name="modifiedbodysystem" value="Lymphatic" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Lymphatic')} /><label>Lymphatic</label>
+
+                        <input type="checkbox" name="modifiedbodysystem" value="Muscular" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Muscular')} /><label>Muscular</label>
+
+                        <input type="checkbox" name="modifiedbodysystem" value="Nervous" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Nervous')} /><label>Nervous</label>
+
+                        <input type="checkbox" name="modifiedbodysystems" value="Renal" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Renal')} /><label>Renal</label>
+
+                        <input type="checkbox" name="modifiedbodysystem" value="Reproductive" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Reproductive')} /><label>Reproductive</label>
+
+                        <input type="checkbox" name="modifiedbodysystem" value="Respiratory" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Respiratory')} /><label>Respiratory</label>
+
+                        <input type="checkbox" name="modifiedbodysystem" value="Skeletal" onChange={this.updateitems} checked={this.state.modifiedbodySystem.includes('Skeletal')} /><label>Skeletal</label>
+
+                        {/* <input
+                        type="text"
+                        value={this.state.modifiedbodySystem}
+                        onChange={(evt) => {
+                            this.setState({
+                                modifiedbodySystem: evt.target.value
+                            });
+                        }}
+                        name="modifiedbodysystem" */}
+                        {/* /> */}
+                    </div>
+                    <br />
+
+                    <h6>Scientific References:</h6>
+                    <textarea rows="6" cols="40"
+                        type="text"
+                        value={this.state.modifiedscienticReferences}
+                        onChange={(evt) => {
+                            this.setState({
+                                modifiedscienticReferences: evt.target.value
+                            });
+                        }}
+                        name="modifiedscientificReferences"
+                    ></textarea> <br />
+
+
+                    <button onClick={() => {
+                        this.updateCase(a);
+                    }}> Confirm </button>
+
+                </div>
+
+            </React.Fragment>
+
+        );
+    }
 
     render() {
+        return (
+
+
+            this.state.data.map(patientsData => {
+                if (patientsData._id === this.state.userBeingEdited) {
+                    return (
+                        <React.Fragment key={patientsData._id}>
+                            <div className="box">{this.renderEditDisplay()}</div>
+                        </React.Fragment>
+                    )
+                } else {
         return <React.Fragment>
             <Tabs activeKey={this.state.key} className="mb-3" id="controlled-tab-example" onSelect={(k) => this.handleSelect(k)} >
                 <Tab eventKey='1' title="Case" >
-                    {this.state.data.map(patientsData => <header className="containerAllCases" key={patientsData._id}>
+                    {/* {this.state.data.map(patientsData => <header className="containerAllCases" key={patientsData._id}> */}
                         <div className="card-group-all-cases">
                             <div className="card-all-cases">
-                                <button
+                                <button className='deletebtn'
                                     onClick={() => {
                                         this.deleteCase(patientsData._id);
                                     }}
                                 >Delete</button>
+                                <button className='editButton' onClick={() => {
+                                                    this.beginEdit(patientsData);
+                                                }}> Edit Case
+                                                </button>
                                 <img src={patientsData.images} className="card-img-top" alt="..." />
                                 <div className="card-body-all-images">
                                     <h5>Case presentation: </h5>
@@ -117,8 +428,8 @@ export default class AllCasesContent extends React.Component {
                             </div>
                         </div>
                         <hr style={{ height: '5px', size: "10", color: 'red' }} />
-                    </header>
-                    )}
+                    {/* </header> */}
+                    {/* )} */}
 
 
 
@@ -136,15 +447,17 @@ export default class AllCasesContent extends React.Component {
                 </Tab>
 
 
-                <Tab eventKey='3' title="Delete Case" >
+                <Tab eventKey='3' title="Edit Case" >
 
                 </Tab>
 
             </Tabs >
-            {this.renderContent()}
+            {/* {this.renderContent()} */}
 
         </React.Fragment >
 
     }
-
+     })
+)
+}
 }
