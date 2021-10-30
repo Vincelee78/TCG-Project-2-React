c2cab4c61384a6dc08ac262ffff1b7c362426e64
Vincelee78 <vincelee7778@gmail.com>
2021-10-30 09:42:38 +0000

    updated css of edit button

--- radiology-webpage/src/components/About.js
+++ radiology-webpage/src/components/About.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import usyd from '../images/usyd.png';
 import work from '../images/radiologywork.jpg';
 import spouse from '../images/changi.jpg';
 import project from '../images/siemens-project.jpg';
-import kneeimplant from '../images/3d-knee-simulation.jpg';
 import spinalimplant from '../images/3d-spinal-implant.jpg';
 
 
@@ -26,7 +24,7 @@ export default class About extends React.Component {
                         <div class='footer' style={{ color: 'wheat' }}>
                             <h5>MedRadiology is an open-edit educational radiology resource that has been primarily compiled by radiologists and radiology trainees from across the world. Our mission is to create the best radiology reference, and to make it available for free, forever.</h5>
                             <h5>My wish is it to become one of the most important medical resources for millions of individuals from around the world, particularly those from low and middle-income regions who do not have access to traditional pay-wall resources. </h5>
-                            
+
                         </div>
                     </div>
                 </div>
@@ -38,26 +36,26 @@ export default class About extends React.Component {
                     </div>
                 </div>
 
-                {/* <div class='background'>
+                <div class='background'>
                     <h4 class=" mx-5 display-6" style={{ color: 'gold' }}>Background</h4>
                     <h4 class=" mx-5 pt-2" style={{ color: 'wheat' }}>I have worked in healthcare in the past couple of years in Australia after my studies there. I spent 13 years in Australia since finishing my final year of high school there. My background is in radiography and medicine. I also have some experience in 3D printing in Siemens Singapore after coming back recently.
                         I am now in the midst of a mid-career transition to information technology.
-                    </h4> */}
-                    {/* <div className='images p-4 d-flex justify-content-around img1'>
-                    <img src={work}/>
-                    <img src={spouse}/>
+                    </h4>
+                    <div className='images p-4 d-flex justify-content-around img1'>
+                        <img src={work} />
+                        <img src={spouse} />
                     </div>
                     <div className='images p-4 d-flex justify-content-around img2'>
-                    <img src={project}/>
-                    <img src={spinalimplant}/>
-                    </div> */}
+                        <img src={project} />
+                        <img src={spinalimplant} />
+                    </div> 
 
-                {/* </div> */}
+                </div>
 
                 <div class='footer'>
-                <div class='background'>
-                    <h4 class=" mx-5 display-6" style={{ color: 'gold' }}>Support MedRadiology</h4>
-                    <h4 class=" mx-5 pt-2" style={{ color: 'wheat' }}>A MedRadiology supporter is someone who values what I am trying to accomplish, and is willing to help with small periodic financial contributions to help build: </h4>
+                    <div class='background'>
+                        <h4 class=" mx-5 display-6" style={{ color: 'gold' }}>Support MedRadiology</h4>
+                        <h4 class=" mx-5 pt-2" style={{ color: 'wheat' }}>A MedRadiology supporter is someone who values what I am trying to accomplish, and is willing to help with small periodic financial contributions to help build: </h4>
                         <ul class=" mx-5 pt-2" style={{ color: 'wheat' }}>
                             <li>innovative educational features</li>
                             <li>enhanced medical information in the radiology reports section</li>
@@ -65,8 +63,8 @@ export default class About extends React.Component {
                             <li>accessibility of radiology information to medical professionals in developing countries</li>
                         </ul>
 
-                    <h5 class=" mx-5 pb-4 pt-2" style={{ color: 'wheat' }}>If you believe in MedRadiology's mission to create the best possible radiology reference and teaching site and make it available to everyone, forever, for free, then please consider becoming a supporter.</h5>
-                </div>
+                        <h5 class=" mx-5 pb-4 pt-2" style={{ color: 'wheat' }}>If you believe in MedRadiology's mission to create the best possible radiology reference and teaching site and make it available to everyone, forever, for free, then please consider becoming a supporter.</h5>
+                    </div>
                 </div>
             </React.Fragment>)
     }
